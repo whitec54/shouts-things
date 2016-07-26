164c0b46cb971cf05fc36af6a6c301edcfe2abe1
Cameron White <whitec54@msu.edu>
2016-07-26 18:24:27 -0500

    4got a ','

--- app.js
+++ app.js
@@ -14,9 +14,9 @@ app.get('/', function(req, res){
 	res.json({message: 'Davids dads...'});
 });
 
-app.post('/' function(req, res){
+app.post('/', function(req, res){
 	res.json(req.body);
-})
+});
 
 app.listen(port, function(){
 	console.log('Magic happens on port ' + port);
