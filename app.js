9f384906316e8732dbeb21bacfa50ea7d6620202
Cameron White <whitec54@msu.edu>
2016-07-26 19:56:47 -0500

    message tweak

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ app.post('/', function(req, res){
 		text = req.body.text,
 		senderName = req.body.name;
 
-	var message = "'"+text+"'? I guess I'm just FUCKING GAY. Isn't that right" +senderName+"?";
+	var message = "'"+text+"'? I guess I'm just FUCKING GAY. Isn't that right, " +senderName+"?";
 
 	if(senderType == 'user'){
 		console.log("IF IS TRUE");
